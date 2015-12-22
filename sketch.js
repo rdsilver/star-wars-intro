25f3ff5bbe47387db00a2da9af93083f762e2d8c
Reed <reedsilverstein@gmail.com>
2015-12-22 14:05:34 -0800

    Readability

--- sketch.js
+++ sketch.js
@@ -1,23 +1,48 @@
 function preload() {
   mySound = loadSound('star_wars_theme.mp3');
+  logo = loadImage("star_wars_logo.gif");
+  dollar_sign_texture = loadImage("dollar_texture.png");
 }
 
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
+  createCanvas(window.innerWidth, window.innerHeight, WEBGL);
   background(0);
-  /*
-  fill(0);
-  textSize(50);
-  textAlign(CENTER);
-  textFont("Helvetica");
-  noStroke();
-  */
+  fill(236, 199, 96);
   mySound.setVolume(0.2);
   mySound.play();
 }
 
+var scale_amount = 0;
+var count = 0;
 function draw() {
-  
-
+  drawLogo();
+  drawDollarSign();
 }
 
+function drawLogo() {
+  push();
+  background(0);
+  texture(logo);
+  scale_amount = 1 - (frameCount/500);
+  if (scale_amount > 0) {
+    scale(scale_amount);
+    plane(width/2.0, height/2.0);
+  }
+  pop();
+}
+
+function drawDollarSign() {
+  push();
+  texture(dollar_sign_texture);
+  rotateX(150);
+  translate(0, height + 100 - count, 0);
+  if (scale_amount < .35) {
+    plane(width/3.0, height/3.0);
+    count += 2;
+  }
+  pop();
+}
+
+function windowResized() {
+  resizeCanvas(window.innerWidth, window.innerHeight);
+}
